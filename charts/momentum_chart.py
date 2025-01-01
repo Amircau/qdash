5fa74863d347bcaeaff088719f94cf40023bf928
Amircau <162605812+Amircau@users.noreply.github.com>
2025-01-01 12:47:28 +0200

    Update momentum_chart.py

--- charts/momentum_chart.py
+++ charts/momentum_chart.py
@@ -13,7 +13,7 @@ def create_momentum_chart(df: pd.DataFrame, ma_periods: List[int]) -> go.Figure:
         x=df.index,
         y=df['close'],
         name='Price',
-        line=dict(color='light_blue')
+        line=dict(color='deepskyblue')
     ))
     for period in ma_periods:
         fig.add_trace(go.Scatter(
@@ -27,7 +27,7 @@ def create_momentum_chart(df: pd.DataFrame, ma_periods: List[int]) -> go.Figure:
         y=df['MOMO_SCORE'],
         name='Momentum Score',
         yaxis='y2',
-        line=dict(color='pink')
+        line=dict(color='indianred')
     ))
     fig.update_layout(
         title="Momentum Score and Moving Averages",
