9552724e590ce017f831ad46c9d8fc76a231ec4e
Amircau <162605812+Amircau@users.noreply.github.com>
2025-01-01 12:39:59 +0200

    Update qdash.py

--- qdash.py
+++ qdash.py
@@ -106,3 +106,6 @@ try:
             primary_data.df
         )
         st.plotly_chart(fig_bollinger, use_container_width=True)
+
+except Exception as e:
+    st.error(f"An error occurred: {str(e)}")
